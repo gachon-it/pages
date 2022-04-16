dd1076c8367f3ba06a391c2151316e2773dd561d
drakejin <dydwls121200@gmail.com>
2022-04-16 20:20:35 +0900

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,152 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <div>
+        <h1>
+          가천대학교 IT 경력개발 모임
+        </h1>
+      </div>
+      <section>
+        <h2>
+          Vision =&gt; 후.학.양.성
+        </h2>
+        <p style={{ marginLeft: '1rem'}}>
+          기본적으로 모임의 방향과 규칙은 모두 가천대학교 학생들의 후학양성에 필요한것만 합니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+      <section>
+        <h2>
+          Rules
+        </h2>
+        <ol>
+          <li>
+            대화명 [이름(닉)/학생,취준,직딩/관심분야,전공/기타] 기본적으로는 실명인데, 실명을 원치 않으면 익명도 괜찮아요
+          </li>
+          <li>
+            누구나 멘토이며 멘티에요
+          </li>
+          <li>
+            질문 하기전에 한번만 더 찾아보거나 고민정도는 해보도록해요
+          </li>
+          <li>
+          새벽시간에도 대화해도 좋아요. 학구열에는 밤낮이 없어요 (알람관리는 본인이 하도록)
+          </li>
+          <li>
+          방의 토픽과 벗어나는 주제로 과열된 토론은 지양해요.
+          </li>
+          <li>
+            너무 둘만 대화하면 저희 외로워요.
+          </li>
+          <li>
+            너무 둘만 대화하면 저희 외로워요.
+          </li>
+          <li>
+            운영제안은 자유롭게 해주세요! 완전 환영해요!
+          </li>
+          <li>
+            이 방에서 기획하고싶은게 있나요? 공익적이라면 일단 진행하세요!
+          </li>
+          <li>
+            부방장은 방 운영에 관심이 많은 사람에게 부여하고있어요! 원하신다면 말씀해주세요~!
+          </li>
+        </ol>
+      </section>
+      <section>
+        <h2>
+          Activity
+        </h2>
+        <h3>
+          정기세미나
+          <a href="https://github.com/gachon-it/seminar" style={{ marginLeft: '0.5rem'}}>[참여링크]</a>
+        </h3>
+        <ul>
+          <li>매 월 마지막주 화요일에 진행합니다</li>
+          <li>IT 경력 분야에 관련된 신변잡기적인 주제로 발표합니다</li>
+        </ul>
+      </section>
+      <section>
+        <h2>
+          Operation Tools
+        </h2>
+        <p style={{ marginLeft: '1rem'}}>
+          모든 운영은 투명하게 공개하고 오픈합니다.
+          기여와 열람은 누구나 할 수 있으며, 의지가 있다면 누구에게나 책임을 부여합니다.
+        </p>
+        <h3>
+          Kakao Open ChatRoom
+          <a href="https://open.kakao.com/o/gxnZ4Z3b" style={{ marginLeft: '0.5rem'}}>[참여링크]</a>
+        </h3>
+        <p style={{ marginLeft: '1rem'}}>주로 신변잡기적인 대화를 나누고, 기본적인 경력개발 관련된 topic으로 대화를 합니다.</p>
+        <h3>
+          Slack
+          <a href="https://join.slack.com/t/gachon-j0y6470/shared_invite/zt-17jgjdych-BM1ngKeKnDtpqKnXLCBvbg" style={{ marginLeft: '0.5rem'}}>[참여링크]</a>
+        </h3>
+        <p style={{ marginLeft: '1rem'}}>카카오톡방보다는 조금 더 지식에 대해 더 깊게 논의할 수 있습니다. 본 모임의 운영관련된 이야기를 합니다.</p>
+        <h3>
+          Github
+          <a href="https://github.com/gachon-it/members" style={{ marginLeft: '0.5rem'}}>[참여링크]</a>
+        </h3>
+        <p style={{ marginLeft: '1rem'}}>경력개발모임에서 주도적으로 관리하는 github입니다. 후학양성에 관련된 repository를 만들고 관리하고 있습니다.</p>
+        <h3>
+          Trello
+          <a href="https://trello.com/invite/gachon6/63f4ce97755b54dce6aa307c88c07add" style={{ marginLeft: '0.5rem'}}>[참여링크]</a>
+        </h3>
+        <p style={{ marginLeft: '1rem'}}>경력개발모임에서 진행하고 있는 모든 task를 관리합니다.</p>
+      </section>
+      <section>
+        <h2>
+          Code of Conduct
+        </h2>
+        <ul>
+          <li>
+            앞선자는 후학을 양성하는데 힘써야 합니다. 앞선자는 선배만을 뜻하진 않습니다.
+          </li>
+          <li>
+            도움을 받았다면, 반드시 받았던 도움을 어떤형태로 라도 다른이에게 도움을 주셔야만 합니다.
+          </li>
+          <li>
+            방내에서 후학을 대려가서 어떠한 학대, 괴롭힘 및 기타 용납할 수 없는 행동의 사례는 철저히 배척하고 사회에 공유하겠습니다. 이에 대한 신고는 정부의 유관부서 또는 <a href="mailto:dydwls121200@gmail.com">dydwls121200@gmail.com</a> / <a href="tel:010-5596-6935">010-5596-6935</a> 로 연락주세요.
+          </li>
+        </ul>
+      </section>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <section>
+        <h2>대충 쓸때없는 이야기</h2>
+        <p style={{ marginLeft: '1rem'}}>
+          컴퓨터미융 11학번 졸업생 조용진이라고 합니다.
+        </p>
+        <p>
+        저는 당근마켓에 검색개발자로 재직중에 있으며, 개발과 스타트업에 대한 이야기를 좋아합니다. 동문의 졸업생들을 모아서 재학생의 후학양성 및 커뮤니케이션을 취지로 카카오톡 오픈 채팅방을 만들었었습니다. 제 재학생때 진로고민으로 고생을 했었는데, 뭔가 경력자분들이나 졸업자분들이 같이 고민해주었으면 하는 부분들이 많았었어요. (결국 인터넷 커뮤니티에 글써보고, 경력자분들 만나가며 고민에 대한 최선의 답을 찾으며 다녔었어요.)
+        </p>
+        <ul>
+          <li>
+            커리어 패스(공부 방향)를 어떻게 짜야하나
+          </li>
+          <li>
+            자소서/포트폴리오 어떻게 짜는게 좋지..?
+          </li>
+          <li>
+            휴학을 하고 실력을 더 키울까?
+          </li>
+          <li>
+            등...
+          </li>
+        </ul>
+        <p>
+          처음에는 이 방에는 5명의 직딩과 10명의 취준 재학생으로 시작했는데, 어떻게 하다보니 지금은 340명이 모인 방으로 성장하게 되었습니다. 인원 구성은 졸업생이 절반이고, 그 절반은 재학생이 들어와있어요.
+        </p>
+        <p>
+          지금은 저 보다 더 뛰어나신분들이 이 방에서 좋은 인사이트를 나눠주고 계시다보니 나가지 않고 계속해서 들어와 주시는거 같아요.
+        </p>
+        <p>
+          진심으로 감사합니다.
+        </p>
+      </section>
     </div>
   );
 }
